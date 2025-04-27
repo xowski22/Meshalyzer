dc179f3b3a5f77eb9a2dc841c3e0106a0d9d360d
xowski22 <xowski22@gmail.com>
2025-04-27 16:18:22 +0200

    fixed some typos

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 from setuptools_rust import RustExtension
 
 setup(
-    name="meshalizer",
+    name="meshalyzer",
     version="0.1.0",
     packages=find_packages(),
-    rust_extensions=[RustExtension("meshalizer.meshalizer", "Cargo.toml", debug=False)],
+    rust_extensions=[RustExtension("meshalyzer.meshalyzer", "Cargo.toml", debug=False)],
     install_requires=[
         "numpy",
         "open3d",
